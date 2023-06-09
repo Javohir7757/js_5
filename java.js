419be4800b18d6e153056cbb98c5bce102972fe5
javohir <javohir@example.com>
2023-06-09 11:19:08 +0300

    bir

---
+++ java.js
@@ -0,0 +1,8 @@
+let son = +prompt("Son kiriting")
+let daraja = +prompt("Sonni darajasini kiriting")
+let sayfullo = 1
+for(let i = 1; i <= daraja ; i++){
+  // ans  *= son
+  sayfullo = sayfullo *son
+}
+console.log(sayfullo)
